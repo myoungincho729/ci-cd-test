d6f6127898d8097d7c4a6a87f1f19fd6267fc639
myoungin <myoungin729@gmail.com>
2023-06-14 23:40:32 +0900

    rds

--- src/main/java/hello/spring/Controller.java
+++ src/main/java/hello/spring/Controller.java
@@ -1,13 +1,34 @@
 package hello.spring;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+
+    private final MemberRepository memberRepository;
+
+    public Controller(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
     
-    @GetMapping("/")
-    public String hello(){
-        return "hello";
+    // @GetMapping("/")
+    // public String hello(){
+    //     return "hello";
+    // }
+    @GetMapping("/{username}/{password}")
+    public Member save(@PathVariable String username, @PathVariable String password){
+        Member member = new Member(null, username, password);
+        Member savedMember = memberRepository.save(member);
+        return savedMember;
+    }
+
+    @GetMapping("/list")
+    public List<Member> findAll(){
+        List<Member> members = memberRepository.findAll();
+        return members;
     }
 }
---
+++ src/main/java/hello/spring/MemberRepository.java
@@ -0,0 +1,9 @@
+package hello.spring;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    
+}
